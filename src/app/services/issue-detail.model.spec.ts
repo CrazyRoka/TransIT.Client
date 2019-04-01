3325e2bce1e7c9f22c20e1a8b7c0447fb5c7fc60
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-04-02 00:23:53 +0300

    Add services/issue-detail.model

---
+++ src/app/services/issue-detail.model.spec.ts
@@ -0,0 +1,7 @@
+import { IssueDetail } from './issue-detail.model';
+
+describe('IssueDetail', () => {
+  it('should create an instance', () => {
+    expect(new IssueDetail()).toBeTruthy();
+  });
+});
---
+++ src/app/services/issue-detail.model.ts
@@ -0,0 +1,16 @@
+import DateTimeFormat = Intl.DateTimeFormat;
+
+export class IssueDetail {
+  ID: number;
+  SUNNARY: string;
+  WARRANTY: number;
+  DEADLINE: DateTimeFormat;
+  STATE_ID: number;
+  ASSIGNED_TO: number;
+  VEHICLE_ID: number;
+  MALFUNCTION_ID: number;
+  CREATE_DATE: DateTimeFormat;
+  MOD_DARE: DateTimeFormat;
+  CREATE_ID: number;
+  MOD_ID: number;
+}
