9f3076f78130864ec95e9dd87652207e4343038d
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-02 20:51:48 +0300

    Substituted core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, LoginComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    NgBootstrapFormValidationModule.forRoot(),
-    NgBootstrapFormValidationModule,
-    NgbModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, NgBootstrapFormValidationModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
+
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [HomeComponent, NavbarComponent, LoginComponent],
+  imports: [CommonModule, NgbModule, ReactiveFormsModule, RouterModule, NgBootstrapFormValidationModule],
+  exports: [HomeComponent, NavbarComponent, LoginComponent]
+})
+export class CoreModule {}
