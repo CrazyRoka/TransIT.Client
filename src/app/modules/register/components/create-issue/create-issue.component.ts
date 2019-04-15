789dd52bc20d8c5ef166cb5025ed552b339101dd
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-16 02:35:39 +0300

    Implemented fetching malfunction data

--- src/app/modules/register/components/create-issue/create-issue.component.ts
+++ src/app/modules/register/components/create-issue/create-issue.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { ErrorMessage } from 'ng-bootstrap-form-validation';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { VehicleService } from '../../services/vehicle.service';
+import { MalfunctionService } from '../../services/malfunction.service';
 import { Vehicle } from '../../models/vehicle';
+import { MalfunctionSubgroup } from '../../models/malfunction-subgroup';
+import { Malfunction } from '../../models/malfunction';
+import { MalfunctionGroup } from '../../models/malfunction-group';
 
 @Component({
   selector: 'app-create-issue',
@@ -13,8 +16,11 @@ export class CreateIssueComponent implements OnInit {
   private issueForm: FormGroup;
 
   vehicles: Vehicle[] = [];
+  malfunctionGroups: MalfunctionGroup[] = [];
+  malfunctionSubgroups: MalfunctionSubgroup[] = [];
+  malfunctions: Malfunction[] = [];
 
-  constructor(private fb: FormBuilder, private vehicleService: VehicleService) {}
+  constructor(private fb: FormBuilder, private vehicleService: VehicleService, private malfunctionService: MalfunctionService) {}
 
   ngOnInit() {
     this.issueForm = this.fb.group({
@@ -26,6 +32,14 @@ export class CreateIssueComponent implements OnInit {
     });
 
     this.vehicleService.getEntities().subscribe(data => (this.vehicles = data));
+    this.malfunctionService.getEntities().subscribe(malfunctions => {
+      const filterFunct = (val: MalfunctionGroup | MalfunctionSubgroup, index: number, a: MalfunctionGroup[] | MalfunctionSubgroup[]) => {
+        return a.findIndex(v => v.name === val.name) === index;
+      };
+      this.malfunctions = malfunctions;
+      this.malfunctionSubgroups = malfunctions.map(m => m.malfunctionSubgroup).filter(filterFunct);
+      this.malfunctionGroups = this.malfunctionSubgroups.map(s => s.malfunctionGroup).filter(filterFunct);
+    });
   }
 
   onSubmit() {
@@ -42,5 +56,43 @@ export class CreateIssueComponent implements OnInit {
     return this.vehicles.map(
       vehicle => `${vehicle.brand} ${vehicle.model} ${vehicle.vincode || ''} ${vehicle.inventoryId || ''} ${vehicle.regNum || ''}`
     );
+  }
+
+  get groupsNames(): string[] {
+    return this.malfunctionGroups.map(g => g.name);
+  }
+
+  get subgroupsNames(): string[] {
+    const subgroups = this.malfunctionSubgroups
+      .filter(val => {
+        const group = this.issueForm.value.malfunctionGroup;
+        if (group === '') {
+          return true;
+        }
+        return val.malfunctionGroup.name === group;
+      })
+      .map(s => s.name);
+    if (subgroups.findIndex(s => s === this.issueForm.value.malfunctionSubgroup) === -1) {
+      this.issueForm.patchValue({ malfunctionSubgroup: '' });
+    }
+    return subgroups;
+  }
+
+  get malfunctionsNames(): string[] {
+    const malfunctions = this.malfunctions
+      .filter(val => {
+        const subgroup = this.issueForm.value.malfunctionSubgroup;
+        if (subgroup === '') {
+          const subgroups = this.subgroupsNames;
+          return subgroups.findIndex(v => v === val.malfunctionSubgroup.name) !== -1;
+        } else {
+          return val.malfunctionSubgroup.name === subgroup;
+        }
+      })
+      .map(m => m.name);
+    if (malfunctions.findIndex(m => m === this.issueForm.value.malfunction) === -1) {
+      this.issueForm.patchValue({ malfunction: '' });
+    }
+    return malfunctions;
   }
 }
