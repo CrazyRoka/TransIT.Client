35abdf4698fb67ebd04add39489679035ea81896
Rostyslav Toch <rokarostuk@gmail.com>
2019-04-24 13:05:53 +0300

    Changed engineer module

--- src/app/modules/admin/services/action-type.sevice.ts
+++ src/app/modules/admin/services/action-type.sevice.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CrudService } from '../../core/services/crud.service';
-import { ActionType } from '../models/action/action-type';
 import { environment } from 'src/environments/environment';
+import { ActionType } from '../models/action/actiontype';
 
 @Injectable({
   providedIn: 'root'
