7aef38d88d3aabe9805fd9e9d0cea81699c194ec
markiyanshvets <markiyan.shvets@gmail.com>
2019-04-25 15:01:21 +0300

    supplier view

--- src/app/modules/admin/component/supplier/create-supplier/create-supplier.component.ts
+++ src/app/modules/admin/component/supplier/create-supplier/create-supplier.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Supplier } from 'src/app/modules/engineer/models/supplier';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-supplier',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-supplier.component.scss']
 })
 export class CreateSupplierComponent implements OnInit {
+  @Output() public createSupplier: EventEmitter<Supplier>; 
+  public supplierForm: FormGroup;
+  public supplier: Supplier;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.createSupplier = new EventEmitter<Supplier>();
+    this.supplierForm = new FormGroup({
+      name: new FormControl(
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(100)])
+      ),
+      description: new FormControl('', Validators.maxLength(512))
+    });
   }
 
+  private newSupplier(): Supplier {
+    return new Supplier({ name: ''});
+  }
+
+  ngOnInit() {
+    this.supplier = this.newSupplier();
+    this.activatedRoute.params.subscribe(res => {
+      this.supplier = new Supplier(res);
+    });
+    $('#createSupplier').on('hidden.bs.modal', () => {
+      $(this)
+        .find('form')
+        .trigger('reset');
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.supplierForm.invalid) {
+      alert('Invalid');
+      return;
+    }
+    this.createSupplier.emit(
+      new Supplier({
+        id: 0,
+        name: this.supplierForm.value.name
+        })
+    );
+  }
 }
--- src/app/modules/admin/component/supplier/supplier.component.ts
+++ src/app/modules/admin/component/supplier/supplier.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Supplier } from 'src/app/modules/engineer/models/supplier';
+import { SupplierService } from 'src/app/modules/engineer/services/supplier.service';
+import { Router } from '@angular/router';
+
+declare const $;
 
 @Component({
   selector: 'app-supplier',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupplierComponent implements OnInit {
 
-  constructor() { }
+  
+  public suppliers: Array<Supplier>;
+  private table: any;
+
+  constructor(
+    private supplierSevice: SupplierService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.table = $('#supplier-table').DataTable({
+      responsive: true,
+      select: {
+        style: 'single'
+      },
+      columns: [
+        { data: 'id', bVisible: false },
+        { title: '', data: 'name', defaultContent: '' },
+      ],
+      paging: true,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
+      }
+    });
+    this.table.on('select', (e, dt, type, indexes) => {
+      const item = this.table.rows( indexes ).data()[0];
+      this.router.navigate(['/admin/supplier/edit', item]);
+    });
+    this.supplierSevice.getEntities().subscribe(suppliers => {
+      this.suppliers = suppliers;
+      this.table.rows.add(this.suppliers);
+      this.table.draw();
+    });
   }
 
 }
