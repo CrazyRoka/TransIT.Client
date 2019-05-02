b172b07f29f9ace8d0e1152665d2c47c94dd0cd9
Iryna Panchuk <rusa1234527@gmail.com>
2019-05-02 12:05:16 +0300

    bug

--- src/app/custom-errors.ts
+++ src/app/custom-errors.ts
@@ -38,7 +38,7 @@ export function matchPassword(AC: AbstractControl) {
   let password = AC.get('password').value;
   let confirmPassword = AC.get('confirmPassword').value;
   if (password != confirmPassword) {
-    AC.get('confirmPassword').setErrors({ MatchPassword: true });
+    AC.get('confirmPassword').setErrors({ matchPassword: true });
   } else {
     return null;
   }
