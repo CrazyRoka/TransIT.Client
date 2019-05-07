327912743d04f5ea69c4a04916cf01ee6dcdcbe2
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-05-07 14:46:11 +0300

    fix

--- src/app/modules/analyst/models/mulfun-subgroup.ts
+++ src/app/modules/analyst/models/mulfun-subgroup.ts
@@ -1,4 +1,5 @@
 import { TEntity } from '../../core/models/entity/entity';
+import { MalfuncGroup } from './mulfun-group';
 
 export class MalfunSubgroup extends TEntity<MalfunSubgroup> {
   name: string;
