78f64cd8bf59a673e7236d56dd411b2e8a17f8f5
Taras Kvasnytsya <taras1kvasnytsya@gmail.com>
2019-05-12 17:09:55 +0300

    fix issues on analyst

--- src/app/modules/shared/components/global-issue/global-issue.component.ts
+++ src/app/modules/shared/components/global-issue/global-issue.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Issue } from '../../models/issue';
 import { IssueService } from '../../services/issue.service';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 declare const $;
 
@@ -11,23 +12,22 @@ declare const $;
   styleUrls: ['./global-issue.component.scss']
 })
 export class GlobalIssueComponent implements OnInit {
-
   public issues: Array<Issue>;
   private table: any;
 
-  constructor(
-    private issueService: IssueService,
-    private router: Router
-  ) { }
+  constructor(private issueService: IssueService, private router: Router) {}
 
   ngOnInit() {
+    this.initTable();
+  }
+
+  protected initTable(): void {
     this.table = $('#issue-table').DataTable({
-      responsive: true,
+      scrollX: true,
       select: {
         style: 'single'
       },
       columns: [
-        { data: 'id', bVisible: false },
         { title: 'Статус', data: 'state.transName', defaultContent: '' },
         { title: 'Поломка', data: 'malfunction.name', defaultContent: '' },
         { title: 'Гарантія', data: 'warranty', defaultContent: '' },
@@ -36,22 +36,19 @@ export class GlobalIssueComponent implements OnInit {
         { title: 'Виконати до', data: 'deadline', defaultContent: '' },
         { title: 'Опис', data: 'summary', defaultContent: '' },
         { title: 'Створено', data: 'createDate', defaultContent: '' },
-        { title: 'Редаговано', data: 'modDate', defaultContent: '' }
+        { title: 'Редаговано', data: 'modDate', defaultContent: '' },
+        { data: 'id', bVisible: false }
       ],
+      processing: true,
+      serverSide: true,
+      ajax: {
+        url: environment.apiUrl + '/datatable/issue',
+        type: 'POST'
+      },
       paging: true,
       language: {
         url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
       }
-    });
-    this.table.on('select', (e, dt, type, indexes) => {
-      const item = this.table.rows(indexes).data()[0];
-      this.router.navigate(['/engineer/issues/edit', new Issue(item)]);
-    });
-    this.issueService.getEntities().subscribe(issues => {
-      this.issues = issues;
-      this.table.rows.add(this.issues);
-      this.table.draw();
-    });
-  }
-
+    });    
+  }  
 }
