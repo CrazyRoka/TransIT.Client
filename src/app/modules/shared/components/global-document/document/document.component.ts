203be0c4b95c5146c46d3e3dbea54162478bbc3c
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-05-18 21:07:45 +0300

    fix visible to analyst

--- src/app/modules/shared/components/global-document/document/document.component.ts
+++ src/app/modules/shared/components/global-document/document/document.component.ts
@@ -20,37 +20,35 @@ export class DocumentComponent implements OnInit {
 
   constructor(private documentService: DocumentService, private router: Router, private toast: ToastrService) {}
 
-  private readonly tableConfig: DataTables.Settings = {
-    responsive: true,
-
-    columns: [
-      { title: 'Назва', data: 'name', defaultContent: '' },
-      { title: 'Опис', data: 'description', defaultContent: '' },
-      { title: 'Змінено', data: 'modDate', defaultContent: '' },
-      { data: 'id', visible: false },
-      { title: 'Дії⠀', orderable: false, visible: this.isVisible }
-    ],
-    processing: true,
-    serverSide: true,
-    ajax: this.ajaxCallback.bind(this),
-    columnDefs: [
-      {
-        targets: -1,
-        data: null,
-        defaultContent: `<button class="first btn" data-toggle="modal" data-target="#editDocument"><i class="fas fa-edit"></i></button>
-         <button class="second btn" data-toggle="modal" data-target="#deleteDocument"><i class="fas fas fa-trash-alt"></i></button>
-         <button class="third btn" data-toggle="modal"><i class="fas fa-info-circle"></i></button>`
-      }
-    ],
-    paging: true,
-    scrollX: true,
-    language: {
-      url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
-    }
-  };
-
   ngOnInit() {
-    this.tableDocument = $('#document-table').DataTable(this.tableConfig);
+    this.tableDocument = $('#document-table').DataTable({
+      responsive: true,
+
+      columns: [
+        { title: 'Назва', data: 'name', defaultContent: '' },
+        { title: 'Опис', data: 'description', defaultContent: '' },
+        { title: 'Змінено', data: 'modDate', defaultContent: '' },
+        { data: 'id', visible: false },
+        { title: 'Дії⠀', orderable: false, visible: this.isVisible }
+      ],
+      processing: true,
+      serverSide: true,
+      ajax: this.ajaxCallback.bind(this),
+      columnDefs: [
+        {
+          targets: -1,
+          data: null,
+          defaultContent: `<button class="first btn" data-toggle="modal" data-target="#editDocument"><i class="fas fa-edit"></i></button>
+           <button class="second btn" data-toggle="modal" data-target="#deleteDocument"><i class="fas fas fa-trash-alt"></i></button>
+           <button class="third btn" data-toggle="modal"><i class="fas fa-info-circle"></i></button>`
+        }
+      ],
+      paging: true,
+      scrollX: true,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
+      }
+    });
     $('#document-table tbody').on('click', '.first', this.selectFirstItem(this));
     $('#document-table tbody').on('click', '.second', this.selectSecondItem(this));
     $('#document-table tbody').on('click', '.third', this.selectThirdItem(this));
