76ce3a720a1d3ba07ac666502116093312e806a6
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-05-21 11:25:48 +0300

    admincan not delete and update itself

--- src/app/modules/admin/component/users/users.component.ts
+++ src/app/modules/admin/component/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user/user';
 import { UserService } from '../../services/user.service';
 import { RoleService } from '../../services/role.service';
+import { AuthenticationService } from 'src/app/modules/core/services/authentication.service';
 
 @Component({
   selector: 'app-users',
@@ -45,13 +46,21 @@ export class UsersComponent implements OnInit {
     ]
   };
 
-  constructor(private service: UserService, private serviceRole: RoleService) {}
+  constructor(
+    private service: UserService,
+    private serviceRole: RoleService,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
     $('#userTable').DataTable(this.tableParams);
     this.service.getEntities().subscribe(users => {
       this.addTableData(users);
     });
+  }
+
+  get userLogin(): string {
+    return this.authService.getLogin();
   }
 
   addTableData(newUsers: User[]) {
@@ -63,13 +72,15 @@ export class UsersComponent implements OnInit {
       i.phoneNumber,
       i.role.transName,
       i.isActive ? 'активний' : 'неактивний',
-      `<button id="find-user-${
-        i.id
-      }" class="btn" data-toggle="modal" data-target="#editUser"><i class="fas fa-edit"></i></button>
+      i.login !== this.userLogin
+        ? `<button id="find-user-${
+            i.id
+          }" class="btn" data-toggle="modal" data-target="#editUser"><i class="fas fa-edit"></i></button>
       <button id="find-user-${
         i.id
       }" class="btn" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash-alt" style="color: darkred"></i>
       </button>`
+        : ''
     ]);
 
     this.dataTable = $('#userTable')
