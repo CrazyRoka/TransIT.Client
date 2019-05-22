f631a3fac2e67a3fce922d0d21cde401dc08c362
markiyanshvets <markiyan.shvets@gmail.com>
2019-05-22 21:57:54 +0300

    edit component

--- src/app/modules/shared/components/supplier/edit-supplier/edit-supplier.component.ts
+++ src/app/modules/shared/components/supplier/edit-supplier/edit-supplier.component.ts
@@ -40,15 +40,33 @@ export class EditSupplierComponent implements OnInit {
     ) { }
 
    ngOnInit() {
+    $('#editSupplier').on('hidden.bs.modal', function() {
+      $(this)
+        .find('form')
+        .trigger('reset');
+    });
     this.supplierForm = this.formBuilder.group({
-      id: '',
-      name: '',
-      fullName: '',
-      edrpou: '',
-      country: '',
-      currency: ''
+      name: ['', Validators.required],
+      fullName: ['', Validators.required],
+      edrpou: [''],
+      country: [''],
+      currency: [''],
+    });    
+    this.countryService.getEntities().subscribe(data => {
+      this.countries = data;
+    });
+    this.currencyService.getEntities().subscribe(data => {
+      this.currencies = data;
     });
   }
+
+  get Countries(): string[] {
+    return this.countries.map(e => e.name);
+  }
+  get Currencies(): string[] {
+    return this.currencies.map(e => e.fullName);
+  }
+
 
   updateData() {
     if (this.supplierForm.invalid) {
@@ -67,43 +85,5 @@ export class EditSupplierComponent implements OnInit {
     };
     this.service.updateEntity(supplier).subscribe(_ => this.updateSupplier.next(supplier));
   }
-
-  //     this.supplierForm = this.formBuilder.group({
-  //     name: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(10)])),
-  //     fullName: new FormControl('', Validators.required),
-  //     edrpou: new FormControl(''),
-  //     country: new FormControl(''),
-  //     currency: new FormControl('')
-  //   });
-  //   this.countryService.getEntities().subscribe(data => {
-  //     this.countries = data;
-  //   });
-  //   this.currencyService.getEntities().subscribe(data => {
-  //     this.currencies = data;
-  //   });
-  // }
-  // get Countries(): string[] {
-  //   return this.countries.map(e => e.name);
-  // }
-  // get Currencies(): string[] {
-  //   return this.currencies.map(e => e.fullName);
-  // }
-  // updateData() {
-  //   if (this.supplierForm.invalid) {
-  //     return;
-  //   }
-  //   this.closeDiv.nativeElement.click();
-  //   const form = this.supplierForm.value;
-  //   const supplier: Supplier = {
-  //     id:0,
-  //     name: form.name,
-  //     fullName: form.fullName,
-  //     edrpou: form.edrpou,
-  //     country: this.countries[this.Countries.findIndex(f => f === form.country)],
-  //     currency: this.currencies[this.Currencies.findIndex(f => f === form.currency)]
-  //   };
-  //   console.log(supplier);
-  //   this.service.updateEntity(supplier).subscribe(data => this.updateSupplier.next(supplier), _ => this.toast.error('Не вдалось редагувати дані', 'Помилка редагування даних'));
-  // }
 }
 
