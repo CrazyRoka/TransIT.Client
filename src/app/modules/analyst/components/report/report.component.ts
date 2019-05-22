a8941fa7eba22b395bab0decc2b59aaa653e4bee
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-05-22 19:47:54 +0300

    fix table

--- src/app/modules/analyst/components/report/report.component.ts
+++ src/app/modules/analyst/components/report/report.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Malfunction } from 'src/app/modules/shared/models/malfunction';
-import { MalfunctionService } from 'src/app/modules/shared/services/malfunction.service';
+import { MalfunctionGroup } from 'src/app/modules/shared/models/malfunction-group';
+import { MalfunctionGroupService } from 'src/app/modules/shared/services/malfunction-group.service';
 
 @Component({
   selector: 'app-report',
@@ -8,47 +8,24 @@ import { MalfunctionService } from 'src/app/modules/shared/services/malfunction.
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
-  private tableMalfunction: DataTables.Api;
-  malfunction: Malfunction;
-  malfunctions: Array<Malfunction>;
+  malfuncGroups: Array<MalfunctionGroup>;
+  tableGroup: any;
+  selectedMalfunctionGroup: MalfunctionGroup;
 
   ngOnInit() {
-    this.tableMalfunction = $('#reportTable').DataTable({
-      responsive: true,
-      columns: [
-        {
-          className: 'details-control',
-          orderable: false,
-          data: null,
-          defaultContent: ''
-        },
-        { title: 'Помилки', data: 'name', defaultContent: '' }
-      ],
-      paging: true,
-      language: {
-        url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
-      }
-    });
-
-    this.malfuncService.getEntities().subscribe(malfunctions => {
-      this.malfunctions = malfunctions;
-      this.tableMalfunction.rows.add(this.malfunctions);
-      this.tableMalfunction.draw();
-    });
-
     function format(d) {
       return (
         '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
         '<tr>' +
         '<td>Full name:</td>' +
         '<td>' +
-        d.description +
+        'd.name' +
         '</td>' +
         '</tr>' +
         '<tr>' +
         '<td>Extension number:</td>' +
         '<td>' +
-        d.title +
+        'd.name' +
         '</td>' +
         '</tr>' +
         '<tr>' +
@@ -59,47 +36,106 @@ export class ReportComponent implements OnInit {
       );
     }
 
-    $(document).ready(function() {
-      var table = $('#example').DataTable({
-        ajax: {
-          url: 'https://ghibliapi.herokuapp.com/films',
-          dataSrc: ''
+    this.tableGroup = $('#example').DataTable({
+      responsive: true,
+      columns: [
+        {
+          title: '',
+          className: 'details-control',
+          orderable: false,
+          data: 'name',
+          defaultContent: ''
         },
-        columns: [
-          {
-            className: 'details-control',
-            orderable: false,
-            data: null,
-            defaultContent: ''
-          },
-          { data: 'title' },
-          { data: 'producer' },
-          { data: 'title' },
-          { data: 'producer' }
-        ],
-        paging: true,
-        language: {
-          url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
+        {
+          title: 'Автобус',
+          data: null,
+          defaultContent: '0'
+        },
+        {
+          title: 'Трамвай',
+          data: null,
+          defaultContent: '1'
+        },
+        {
+          title: 'Тролейбус',
+          data: null,
+          defaultContent: '2'
+        },
+        {
+          title: 'Електробус',
+          data: null,
+          defaultContent: '3'
         }
-      });
-
-      // Add event listener for opening and closing details
-      $('#example tbody').on('click', 'td.details-control', function() {
-        var tr = $(this).closest('tr');
-        var row = table.row(tr);
-
-        if (row.child.isShown()) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass('shown');
-        } else {
-          // Open this row
-          row.child(format(row.data())).show();
-          tr.addClass('shown');
-        }
-      });
+      ],
+      paging: true,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.19/i18n/Ukrainian.json'
+      }
     });
+
+    this.tableGroup.on('select', (e, dt, type, index) => {
+      const item = this.tableGroup.rows(index).data()[0];
+      this.selectedMalfunctionGroup = item;
+    });
+
+    this.malfuncGroupService.getEntities().subscribe(malfuncGroups => {
+      this.malfuncGroups = malfuncGroups;
+      this.tableGroup.rows.add(this.malfuncGroups);
+      this.tableGroup.draw();
+    });
+
+    $('#example tbody').on('click', 'td.details-control', this.showRow(this));
+    // function() {
+    //   var tr = $(this);
+    //   var row = this.tableGroup.row(tr);
+    //   if (row.child.isShown()) {
+    //     row.child.hide();
+    //     tr.removeClass('shown');
+    //   } else {
+    //     row.child(format(row.data())).show();
+    //     console.dir(row);
+    //     console.dir();
+    //     tr.addClass('shown');
+    //   }
+    // });
   }
 
-  constructor(private malfuncService: MalfunctionService) {}
+  format(d) {
+    return (
+      '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+      '<tr>' +
+      '<td>Full name:</td>' +
+      '<td>' +
+      'd.name' +
+      '</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Extension number:</td>' +
+      '<td>' +
+      'd.name' +
+      '</td>' +
+      '</tr>' +
+      '<tr>' +
+      '<td>Extra info:</td>' +
+      '<td>And any further details here (images etc)...</td>' +
+      '</tr>' +
+      '</table>'
+    );
+  }
+
+  private showRow(component: any) {
+    return function() {
+      var tr = $(this).closest('tr');
+      var row = component.tableGroup.row(tr);
+      if (row.child.isShown()) {
+        row.child.hide();
+        tr.removeClass('shown');
+      } else {
+        row.child(component.format(row.data())).show();
+        tr.addClass('shown');
+      }
+    };
+  }
+
+  constructor(private malfuncGroupService: MalfunctionGroupService) {}
 }
