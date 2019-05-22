19a1821e3fa53789596b3c5d0566d68d03d380f5
Danylo Dudok <danylo.dudok@protonmail.com>
2019-05-22 18:00:17 +0300

    transition

--- src/app/modules/engineer/components/issue-log-components/edit-issue-log/edit-issue-log.component.ts
+++ src/app/modules/engineer/components/issue-log-components/edit-issue-log/edit-issue-log.component.ts
@@ -9,11 +9,11 @@ import { ActionType } from 'src/app/modules/shared/models/action-type';
 import { State } from 'src/app/modules/shared/models/state';
 import { IssueService } from 'src/app/modules/shared/services/issue.service';
 import { IssuelogService } from 'src/app/modules/shared/services/issuelog.service';
-import { ActionTypeService } from 'src/app/modules/shared/services/action-type.service';
-import { StateService } from 'src/app/modules/shared/services/state.service';
 import { SupplierService } from 'src/app/modules/shared/services/supplier.service';
 import { DocumentService } from 'src/app/modules/shared/services/document.service';
 import { Document } from 'src/app/modules/shared/models/document';
+import { TransitionService } from 'src/app/modules/shared/services/transition.service';
+import { Issue } from 'src/app/modules/shared/models/issue';
 
 @Component({
   selector: 'app-edit-issue-log',
@@ -33,45 +33,99 @@ export class EditIssueLogComponent implements OnInit {
   existingDocuments: Array<Document> = new Array<Document>();
   shownDocuments: Array<Document> = new Array<Document>();
 
+  private selectedIssue: Issue;
+
+  componentState: any = {
+    isDisabled: true,
+    states: []
+  };
+
+  get isStatesDisabled(): boolean {
+    return !this.issueLog.actionType
+      || !this.states
+      || !this.states.length;
+  }
+
   constructor(
     private router: Router,
     private issueService: IssueService,
     private issueLogService: IssuelogService,
-    private actionTypeService: ActionTypeService,
-    private stateService: StateService,
+    private transitionService: TransitionService,
     private supplierService: SupplierService,
     private documentService: DocumentService,
     private toast: ToastrService
   ) {
     this.documents = new Array<Document>();
     this.issueLogForm = new FormGroup({
-      state: new FormControl('', Validators.compose([Validators.required, Validators.nullValidator])),
-      actionType: new FormControl('', Validators.compose([Validators.required, Validators.nullValidator])),
+      // newState: new FormControl('', Validators.compose([Validators.required, Validators.nullValidator])),
+      // actionType: new FormControl('', Validators.compose([Validators.required, Validators.nullValidator])),
       expenses: new FormControl('', Validators.nullValidator),
       summary: new FormControl('', Validators.compose([Validators.nullValidator, Validators.maxLength(512)])),
-      supplier: new FormControl('', Validators.nullValidator),
+      // supplier: new FormControl(''),
       deadLine: new FormControl('')
     });
   }
 
   ngOnInit() {
-    const selectedIssue = this.issueService.selectedItem;
+    this.selectedIssue = this.issueService.selectedItem;
     this.issueLog = new IssueLog({
       id: 0,
       description: '',
       expenses: 0,
-      actionType: new ActionType({}),
+      actionType: null,
       supplier: new Supplier({}),
-      issue: selectedIssue,
-      oldState: selectedIssue.state,
-      newState: selectedIssue.state
+      issue: this.selectedIssue,
+      oldState: this.selectedIssue.state,
+      newState: this.selectedIssue.state
     });
     if (!this.issueLog.issue) {
       this.router.navigate(['/engineer/issues']);
     }
-    this.actionTypeService.getEntities().subscribe(actions => (this.actionTypes = actions));
-    this.stateService.getEntities().subscribe(states => (this.states = states));
-    this.supplierService.getEntities().subscribe(suppliers => (this.suppliers = suppliers));
+    this.issueLog.newState = this.selectedIssue.state;
+    this.supplierService.getEntities().subscribe(suppliers => this.suppliers = suppliers);
+    this.loadActionTypes();
+  }
+
+  private loadActionTypes(): void {
+    this.transitionService.getFilteredEntities({
+      filters: [
+      {
+        entityPropertyPath: 'fromState.id',
+        value: this.selectedIssue.state.id,
+        operator: '=='
+      }
+    ]
+    }).subscribe(transitions => {
+      this.actionTypes = this.distinct(transitions.data.map(t => t.actionType));
+      this.issueLog.newState = this.distinct(transitions.data.map(t => t.toState))[0];
+    });
+  }
+
+  private loadNewStates(): void {
+    if (this.issueLog.actionType) {
+      this.transitionService.getFilteredEntities({
+        filters: [
+        {
+          entityPropertyPath: 'actionType.id',
+          value: this.issueLog.actionType.id,
+          operator: '=='
+        }
+      ]
+      }).subscribe(transitions => {
+        this.states = this.distinct(transitions.data.map(t => t.toState));
+        this.issueLog.newState = this.states[0];
+      });
+    }
+  }
+
+  private distinct(array: Array<any>): Array<any> {
+    const res = [];
+    array.forEach(x => {
+      if (!res.find(i => i.id === x.id)) {
+        res.push(x);
+      }
+    });
+    return res;
   }
 
   assignDocument(entity: Document): void {
@@ -113,6 +167,8 @@ export class EditIssueLogComponent implements OnInit {
 
   assignActionType(entity: ActionType): void {
     this.issueLog.actionType = entity;
+    this.loadNewStates();
+    this.componentState.isDisabled = false;
   }
 
   assignState(entity: State): void {
@@ -137,6 +193,9 @@ export class EditIssueLogComponent implements OnInit {
     this.issueLog.issue.deadline = this.issueLogForm.value.deadline
       ? this.issueLog.issue.deadline
       : this.issueLogForm.value.deadline;
+    this.issueLog.supplier = this.issueLog.supplier.id
+      ? this.issueLog.supplier
+      : null;
     this.issueLogService.addEntity(this.issueLog).subscribe(res => {
       if (this.documents.length) {
         this.documents.forEach(d => {
@@ -149,7 +208,9 @@ export class EditIssueLogComponent implements OnInit {
           this.documentService.addEntity(d).subscribe()
           );
       }
-      this.router.navigate(['/engineer/issue-logs']).then(_ => this.toast.success('', 'Обробку зроблено'));
+      this.router.navigate(['/engineer/issue-logs'])
+        .then(_ => this.toast.success('', 'Обробку зроблено'))
+        .catch(_ => this.toast.error('', 'Обробку не зроблено'));
     });
   }
 }
--- src/app/modules/engineer/engineer.module.ts
+++ src/app/modules/engineer/engineer.module.ts
@@ -18,7 +18,6 @@ import { NestedIssueLogsComponent } from './components/issue-log-components/nest
 import { IssueLogAssigneesComponent } from './components/issue-log-components/issue-log-assignees/issue-log-assignees.component';
 import { IssueLogSuppliersComponent } from './components/issue-log-components/issue-log-suppliers/issue-log-suppliers.component';
 import { IssueLogActionTypeComponent } from './components/issue-log-components/issue-log-action-type/issue-log-action-type.component';
-import { IssueLogStateComponent } from './components/issue-log-components/issue-log-state/issue-log-state.component';
 import { IssueLogDocumentsComponent } from './components/issue-log-components/issue-log-documents/issue-log-documents.component';
 import { DocumentComponent } from '../shared/components/global-document/document/document.component';
 import { CreateDocumentComponent } from './components/create-document/create-document.component';
@@ -58,7 +57,6 @@ const routes: Routes = [
     IssueLogAssigneesComponent,
     IssueLogSuppliersComponent,
     IssueLogActionTypeComponent,
-    IssueLogStateComponent,
     IssueLogDocumentsComponent,
     CreateDocumentComponent
   ]
