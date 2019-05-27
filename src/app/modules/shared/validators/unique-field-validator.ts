e886abde1e8d6b0ca19a8f43c4fe41f28a91301a
Rostyslav Toch <rokarostuk@gmail.com>
2019-05-27 15:11:29 +0300

    Removed comment

--- src/app/modules/shared/validators/unique-field-validator.ts
+++ src/app/modules/shared/validators/unique-field-validator.ts
@@ -1,25 +1,8 @@
 import { TEntity } from '../../core/models/entity/entity';
 import { CrudService } from '../../core/services/crud.service';
-import { FormGroup, AbstractControl } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 import { timer } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
-
-// export const uniqueFieldValidator = <T extends TEntity<T>>(
-//   service: CrudService<T>,
-//   fieldName: string,
-//   time: number = 500
-// ) => {
-//   return (group: FormGroup) => {
-//     return timer(time).pipe(
-//       switchMap(() =>
-//         service.getFilteredEntities({
-//           filters: [{ entityPropertyPath: group[fieldName], operator: '==', value: group[fieldName].value }]
-//         })
-//       ),
-//       map(res => (res.data.length === 0 ? null : { uniqueViolation: true }))
-//     );
-//   };
-// };
 
 export class UniqueFieldValidator {
   static createValidator<T extends TEntity<T>>(service: CrudService<T>, fieldName: string, time: number = 500) {
