7176bcd5def91d35f5698c5d826f43396e0eddf7
yuriimochulskyi <yurii.mochulskyi@gmail.com>
2019-06-03 12:46:31 +0300

    add delete location

--- src/app/modules/admin/component/locations/delete-location/delete-location.component.ts
+++ src/app/modules/admin/component/locations/delete-location/delete-location.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Location } from 'src/app/modules/shared/models/location';
+import { LocationService } from 'src/app/modules/shared/services/location.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-delete-location',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-location.component.scss']
 })
 export class DeleteLocationComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('close') closeDiv: ElementRef;
+  @Input() location: Location;
+  @Output() deleteLocation = new EventEmitter<Location>();
+  constructor(private service: LocationService, private toast: ToastrService) { }
 
   ngOnInit() {
   }
 
+  delete() {
+    this.closeDiv.nativeElement.click();
+    this.service
+      .deleteEntity(this.location.id)
+      .subscribe(
+        data => this.deleteLocation.next(this.location),
+        _ => this.toast.error('Не вдалось видалити місцезнаходження', 'Помилка видалення місцезнаходження')
+      );
+  }
 }
